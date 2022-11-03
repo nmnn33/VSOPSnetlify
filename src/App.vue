dab3d449d8140055eb25fbae0521568f20b86c16
olentoTM <jantti.markus@hotmail.fi>
2022-11-03 16:43:56 +0200

    Tilauslistan alku

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import HeaderBar from './components/HeaderBar.vue';
+  import OrderCard from './components/OrderCard.vue';
+
+
+</script>
+
+<template>
+  <div class="container">
+    <HeaderBar />
+
+    <div class="row">
+        <OrderCard v-for="order in orders" :key="orders._id" :orders="order" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        orders: []
+      }
+    },
+    mounted(){
+      fetch("http://localhost:3000/findOrders") //Testausta varten hakee localhostin kautta. Kannattaa myöhemmin muuttaa.
+      .then(res => res.json())
+      .then(data => this.orders = data)
+      .catch(err => console.log(err.message))
+    }
+  }
+</script>
---
+++ src/components/OrderCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Order ID: {{orders._id}}</h5>
+        <div class="card-content">
+          <p class="card-text">
+            Order Date: {{orders.orderDate}} <br>
+            Customer Name: {{orders.firstName}} {{orders.lastName}} <br>
+            Phone: {{orders.phoneNumber}} <br>
+            Email: {{orders.customerEmail}}
+          </p>
+          <a href="#" class="btn btn-primary orderCardBtn"><i class="bi bi-eye-fill"></i> Info</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    props: {
+      orders: Object
+    }
+  }
+</script>
